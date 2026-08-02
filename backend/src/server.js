d3b6def0b94dbe6b5e66885bf4891d8028fc1971
malik2322 <dasamalik18@gmail.com>
2026-08-02 13:36:57 +0500

    Feat: preparing it for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,31 +1,33 @@
-import express from 'express';
+import express from "express";
 import dotenv from "dotenv";
-import cors from "cors"
+import cors from "cors";
+import path from "path";
 
 import notesRoutes from "./routes/notesRoutes.js";
-import {connectDB} from "./config/db.js";
+import { connectDB } from "./config/db.js";
 import rateLimiter from "./middleware/rateLimiter.js";
 
 dotenv.config();
 
 //  console.log("MongoDB connection string:", process.env.MONGO_URL);
 
-
 // const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 5001;
-
-
+const __dirname = path.resolve(); // Get the current directory name
 
 // Middleware: is a function that has access to the request object (req), the response object (res), and the next middleware function in the application’s request-response cycle. The next middleware function is commonly denoted by a variable named next.
 
+if (process.env.NODE_ENV !== "production") {
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+      // credentials: true
+    }),
+  );
+}
 
-app.use(cors({
-  origin: "http://localhost:5173",  
-  // credentials: true 
-
-}))
-// middleware to parse JSON bodies eg: req.body 
+// middleware to parse JSON bodies eg: req.body
 app.use(express.json());
 
 // custom simple middleware to log incoming requests
@@ -35,13 +37,20 @@ app.use(express.json());
 // })
 app.use(rateLimiter); // Apply the rate limiter middleware to all routes
 
-
 app.use("/api/notes", notesRoutes);
 
+// app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 // what is endpoint? endpoint is the combination of the route(URL) and the method(HTTP method). That let the client identify a specific resource. For example, GET /api/notes is an endpoint. POST /api/notes is another endpoint.
 connectDB().then(() => {
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
   });
 });
-
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,9 +1,9 @@
 
-
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "https://your-production-api-url.com/api";
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:5001/api"
+    baseURL: BASE_URL
 })
 
 export default api;
